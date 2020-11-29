de3ea3b1d958b2744a3aee6c41f55e3d682be529
FlashHit <56718716+FlashHit@users.noreply.github.com>
2020-11-29 19:31:28 +0100

    Fix the preview cameras for B

--- ext/Client/otherStuff.lua
+++ ext/Client/otherStuff.lua
@@ -29,6 +29,17 @@ flagProgress = ResourceManager:RegisterInstanceLoadHandler(Guid('37281D8D-BB5A-1
 	tagCompData.iconSize = 0.0
 	tagCompData.drawDistance = 48.0 
 	tagCompData.scaleDistance = 0.0
+	
+end)
+
+-- Fix the preview cameras for B
+flagCamera = ResourceManager:RegisterInstanceLoadHandler(Guid('C98941E6-B300-4D5F-BC2E-ECA03DBF855D'), Guid('C4F7368E-4783-C08E-CBB9-66521D0AD158'), function(instance)
+	if SharedUtils:GetLevelName() == "Levels/MP_001/MP_001" and SharedUtils:GetCurrentGameMode() == "ConquestSmall0" then
+		instance = InterfaceDescriptorData(instance)
+		instance:MakeWritable()
+		instance.fields[3].value = 'LinearTransform ((0.98216074705124, 0.048190776258707, 0.18176361918449),(-0.0067436555400491, 0.97500890493393, -0.22206361591816),(-0.18792253732681, 0.21687638759613, 0.95794034004211),(4.077354431152, -0.392906188965, 22.938781738281))'
+		instance.fields[4].value = 'LinearTransform ((-0.94940805435181, 0.048190597444773, 0.31032577157021),(0.10450667142868, 0.98031860589981, 0.16749280691147),(-0.29614654183388, 0.19145013391972, -0.93575859069824),(4.23112487793, 0.000360595703, -17.490152359009))'
+	end
 end)
 
 end
