5025061bf56bd1fa9998f7160842f51c80c759aa
FlashHit <56718716+FlashHit@users.noreply.github.com>
2020-12-06 17:05:47 +0100

    comment out Hipfire Fix

--- ext/Client/aimglitchfix.lua
+++ ext/Client/aimglitchfix.lua
@@ -16,19 +16,21 @@ weaponFiring = Events:Subscribe('WeaponFiring:Update', function(weaponFiring)
 		weaponFiring.gunSway.dispersionAngle = 2.5 --i should get the default value of that gun instead of choosing one value for all guns
 		trigger = false
 	end
-	if weaponFiring.gunSway ~= nil and weaponFiring.gunSway.dispersionAngle < 2.5 then --same here, it should be "if smaller then default spread"
+	--if weaponFiring.gunSway ~= nil and weaponFiring.gunSway.dispersionAngle < 2.5 then --same here, it should be "if smaller then default spread"
 	--[[if InputManager:IsDown(InputConceptIdentifiers.ConceptZoom) == false then
 			weaponFiring.gunSway.dispersionAngle = 2.5 --same here
 		end]]
 		--this one could work better if I just had the right value otherwise it makes the default crosshairs, which is smaller, bigger...
+		
+		--[[ HIPFIRE FIX
 		if InputManager:IsDown(InputConceptIdentifiers.ConceptFire) and InputManager:IsDown(InputConceptIdentifiers.ConceptZoom) then
 			hipFireTrigger = true
 		end
 		if InputManager:IsDown(InputConceptIdentifiers.ConceptFire) and InputManager:IsDown(InputConceptIdentifiers.ConceptZoom) == false and hipFireTrigger == true then
 			hipFireTrigger = false
 			weaponFiring.gunSway.dispersionAngle = 2.5 --i should get the default value of that gun instead of choosing one value for all guns
-		end
-	end
+		end]]
+	--end
 end)
 engineMessage = Events:Subscribe('Engine:Message', function(message)
 	if message.type == 638585794 then
